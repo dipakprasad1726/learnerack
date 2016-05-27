d1bd6244cee0abe0a16526db4d4dd53eb60d2f26
Bhushan Gadekar <bhushangadekar01@gmail.com>
2016-05-27 20:48:05 +0530

    Added gitignore file

--- README.md
+++
@@ -1,2 +0,0 @@
-# learnerack
-Leanrning and developement
--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
- // Connects to Our Database 
- $conn = new mysqli('localhost', 'root', 'root123', 'learnerack');
- if ($conn->connect_error)
- {
-     die("Connection failed: " . $conn->connect_error);
- }	
- ?> 
